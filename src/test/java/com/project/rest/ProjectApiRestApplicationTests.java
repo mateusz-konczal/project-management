e6193ee4b836f0898bdc42ca02996000ef82874b
Mateusz Kończal <matkon004@utp.edu.pl>
2021-03-22 03:25:06 +0100

    PZ-4: Generated the project structure

---
+++ src/test/java/com/project/rest/ProjectApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
