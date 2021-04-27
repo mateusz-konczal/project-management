82d4cbbf8dd9ddb4d370495c2ffe45273389a48e
Mateusz Kończal <matkon004@utp.edu.pl>
2021-04-27 06:04:53 +0200

    Added annotations to the repositories

--- src/main/java/com/project/repositories/TasksRepository.java
+++ src/main/java/com/project/repositories/TasksRepository.java
@@ -2,9 +2,11 @@ package com.project.repositories;
 
 import com.project.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TasksRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByProjectID(Long projectID);
 }
--- src/main/java/com/project/repositories/UsersRepository.java
+++ src/main/java/com/project/repositories/UsersRepository.java
@@ -2,9 +2,11 @@ package com.project.repositories;
 
 import com.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsersRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
